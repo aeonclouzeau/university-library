f773428be01bf4d452104eb34a3c40ca435965cb
Aeon Clouzeau <aeonruizc@gmail.com>
2025-10-05 14:04:25 -0600

    add global styling tokens and fonts

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 import { Button } from "@/components/ui/button";
 
-export default function Home() {
-  return (
-    <>
-      <Button>Click Me</Button>
-    </>
-  );
-}
+const Home = () => (
+  <>
+    <Button>Click Me</Button>
+  </>
+);
+
+export default Home;
